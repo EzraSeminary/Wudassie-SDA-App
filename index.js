c3b66bd94431b5016f857213c1a09806b17124bb
AmanWorku <amexworku2014@gmail.com>
2026-02-04 22:42:43 +0300

    Fix error

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
+// Run font setup first so defaultProps are applied before other modules import Text
+import './setupFonts';
 import App from './App';
 import {name as appName} from './app.json';
 
